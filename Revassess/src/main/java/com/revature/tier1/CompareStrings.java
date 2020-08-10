de79e9f1e72e7bc6b55eed6f1d5639f6c3843f70
dan39radke <dan39radke@gmail.com>
2020-08-10 14:20:15 -0400

    compareStrings

--- Revassess/src/main/java/com/revature/tier1/CompareStrings.java
+++ Revassess/src/main/java/com/revature/tier1/CompareStrings.java
@@ -3,6 +3,15 @@ package com.revature.tier1;
 public class CompareStrings {
 
     public static boolean compareStrings(String s1, String s2){
+        for(i=0;i < s2.length(); i++) {
+            if(s1.charAt[i]==s2.charAt[i]) {
+                i++;
+                return true;
+            }
+        }
+
+
         return false;
     }
 }
+
