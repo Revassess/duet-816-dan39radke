2494ef6b3e8410715a6fa2f14883fdf63ea96132
dan39radke <dan39radke@gmail.com>
2020-08-10 14:48:05 -0400

    tier1

--- Revassess/src/main/java/com/revature/tier1/CompareStrings.java
+++ Revassess/src/main/java/com/revature/tier1/CompareStrings.java
@@ -2,15 +2,11 @@ package com.revature.tier1;
 
 public class CompareStrings {
 
-    public static boolean compareStrings(String s1, String s2){
-        for(i=0;i < s2.length(); i++) {
-            if(s1.charAt[i]==s2.charAt[i]) {
-                i++;
-                return true;
-            }
+    public static boolean compareStrings(String s1, String s2) {
+
+        if (s1.equals(s2)) {
+            return true;
         }
-
-
         return false;
     }
 }
--- Revassess/src/main/java/com/revature/tier1/SumOverArray.java
+++ Revassess/src/main/java/com/revature/tier1/SumOverArray.java
@@ -3,6 +3,16 @@ package com.revature.tier1;
 public class SumOverArray {
 
 	public static int IterateAndSum(int[] arr) {
-		return 0;
+
+		if (arr == null) {
+			return 0;
+		} else {
+			int sum = 0;
+			for (int num : arr) {
+				sum += num;
+			}
+			return sum;
+
+		}
 	}
 }
