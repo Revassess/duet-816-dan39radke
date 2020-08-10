db8ff76708f394e676e480279bcbe168add6750d
dan39radke <dan39radke@gmail.com>
2020-08-10 17:20:32 -0400

    q3 tier3

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+A
+Revassess/pom.xml,6\a\6a6bb6ba8f15fc9db9099732acca0cf6a08c7876
+k
+;Revassess/src/test/java/com/revature/config/TestConfig.java,9\0\9006cd03b4562f637184d47735dcb1987723a09a
--- Revassess/src/main/java/com/revature/config/ConnectionUtil.java
+++ Revassess/src/main/java/com/revature/config/ConnectionUtil.java
@@ -16,15 +16,15 @@ public class ConnectionUtil {
 	private static ConnectionUtil cu;
 	
 	// add your jdbc url
-	public static final String URL = "";
+	public static final String URL = "jdbc:postgresql://database-1.ckdonqvjc7hf.us-east-2.rds.amazonaws.com:5432/database_revature720";
 	// add your jdbc username
-	public static final String USERNAME = "";
+	public static final String USERNAME = "dan39radke";
 	// add your jdbc password
-	public static final String PASSWORD = "";
+	public static final String PASSWORD = "51ldLU3gSw2c";
 	// name of the created stored procedure in tier 3
 	public static final String TIER_3_PROCEDURE_NAME = "";
 	// name of the created sequence in tier 3
-	public static final String TIER_3_SEQUENCE_NAME = "";
+	public static final String TIER_3_SEQUENCE_NAME = "incrementby3";
 
 	// implement this method to connect to the db and return the connection object
 	public Connection connect(){
